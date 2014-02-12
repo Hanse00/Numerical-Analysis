0a46152e5337f3f01e17fe190dedc45d3f42905a
Philip Hansen <me@philiphansen.dk>
2014-02-12 18:40:25 +0100

    Sum x and y values

--- linear-regression.py
+++ linear-regression.py
@@ -5,7 +5,10 @@ Return liniear expression calculated by the least squares method.
 
 import sys
 
+#Define global values
 pairs = []
+sumx = 0
+sumy = 0
 
 #Number of pairs is half the number of values, excluding script name
 pair_num = (len(sys.argv) - 1) / 2
@@ -17,9 +20,18 @@ for i in range(pair_num):
     y = x + 1
 
     #Debug code
-    #print "(" + str(sys.argv[x]) + ", " + str(sys.argv[y]) + ")"
+    print "(" + str(sys.argv[x]) + ", " + str(sys.argv[y]) + ")"
 
     #Save input as two dimensional list
     pairs.append([sys.argv[x],sys.argv[y]])
 
+#Debug code
 print pairs
+
+#Sum x and y values
+for i in pairs:
+    sumx = sumx + int(i[0])
+    sumy = sumy + int(i[1])
+
+print "Sum of x values: " + str(sumx)
+print "Sum of y values: " + str(sumy)
