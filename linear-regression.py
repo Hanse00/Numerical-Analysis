b92f909bd567700e9429e3f1412d2402d8a0924e
Philip Hansen <me@philiphansen.dk>
2014-02-12 18:17:48 +0100

    Added comments for clarity

--- linear-regression.py
+++ linear-regression.py
@@ -5,10 +5,12 @@ Return liniear expression calculated by the least squares method.
 
 import sys
 
+#Number of pairs is half the number of values, excluding script name.
 pair_num = (len(sys.argv) - 1) / 2
 print "Pairs inputted: " + str(pair_num)
 
 for i in range(pair_num):
+    #Every other value is an x value, starting from index 1.
     x = (i * 2) + 1
     y = x + 1
 
