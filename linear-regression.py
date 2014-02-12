76943097138f606a4adddfd5d3dcdabe84666f8f
Philip Hansen <me@philiphansen.dk>
2014-02-12 18:24:11 +0100

    Save input as list

--- linear-regression.py
+++ linear-regression.py
@@ -5,13 +5,21 @@ Return liniear expression calculated by the least squares method.
 
 import sys
 
-#Number of pairs is half the number of values, excluding script name.
+pairs = []
+
+#Number of pairs is half the number of values, excluding script name
 pair_num = (len(sys.argv) - 1) / 2
 print "Pairs inputted: " + str(pair_num)
 
 for i in range(pair_num):
-    #Every other value is an x value, starting from index 1.
+    #Every other value is an x value, starting from index 1
     x = (i * 2) + 1
     y = x + 1
 
-    print "(" + str(sys.argv[x]) + ", " + str(sys.argv[y]) + ")"
+    #Debug code
+    #print "(" + str(sys.argv[x]) + ", " + str(sys.argv[y]) + ")"
+
+    #Save input as two dimensional list
+    pairs.append([sys.argv[x],sys.argv[y]])
+
+print pairs
