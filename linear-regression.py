12a2b79cd1ddb33d977b6cbfd488a8fccf0f56ef
Philip Hansen <me@philiphansen.dk>
2014-02-12 18:05:55 +0100

    Output number of pair arguments

---
+++ linear-regression.py
@@ -0,0 +1,11 @@
+"""
+Take a two dimensional list of x,y coordinates.
+Return liniear expression calculated by the least squares method.
+"""
+
+import sys
+
+pair_num = (len(sys.argv) - 1) / 2
+print "Pairs inputted: " + str(pair_num)
+
+print str(sys.argv)
