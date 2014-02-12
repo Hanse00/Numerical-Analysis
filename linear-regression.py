25e514c4d0e3dcafc9519a2c9ccdc5423c48f60f
Philip Hansen <me@philiphansen.dk>
2014-02-12 18:14:08 +0100

    Print inputted value pairs

--- linear-regression.py
+++ linear-regression.py
@@ -8,4 +8,8 @@ import sys
 pair_num = (len(sys.argv) - 1) / 2
 print "Pairs inputted: " + str(pair_num)
 
-print str(sys.argv)
+for i in range(pair_num):
+    x = (i * 2) + 1
+    y = x + 1
+
+    print "(" + str(sys.argv[x]) + ", " + str(sys.argv[y]) + ")"
