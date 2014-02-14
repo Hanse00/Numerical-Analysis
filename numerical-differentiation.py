0fbcb581913caa17ac5f72e6480db2603fdab64e
Philip Hansen <me@philiphansen.dk>
2014-02-14 14:12:07 +0100

    moved calculation to function

--- numerical-differentiation.py
+++ numerical-differentiation.py
@@ -1,15 +1,20 @@
 import sys
 from math import *
 
+def calc(function, x):
+    return eval(function)
+
 function = ""
 x = 0.0
+h = 0.0
 
-if sys.argv < 3:
+if len(sys.argv) < 4:
     print "Too little arguments!"
-    print "Usage: numerical-differentation.py <function> <x value>"
+    print "Usage: numerical-differentation.py <function> <x value> <h value>"
     sys.exit(-1)
 else:
     function = sys.argv[1]
     x = float(sys.argv[2])
-    y = eval(function)
-    print y
+    h = float(sys.argv[3])
+
+    print calc(function, x)
